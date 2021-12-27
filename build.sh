5aabc8cf71c6b803144fe7747f7669dbfef93ab5
Huseyin <huseyin.alecakir@afiniti.com>
2021-12-28 00:35:36 +0300

    Add config toml

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-docker login --username halecakir
 docker build . -t turkish_nlp
 docker tag turkish_nlp:latest halecakir/turkish_nlp_toolkit
+docker login --username halecakir
 docker push halecakir/turkish_nlp_toolkit
