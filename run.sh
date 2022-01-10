bc3f9938f80e14f4ca0574c414fc86b08eb1e593
Huseyin <huseyin.alecakir@afiniti.com>
2022-01-10 21:00:52 +0300

    Release update

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 docker pull halecakir/turkish_nlp_toolkit
-docker run -p 10000:8501 -d --name turkish_nlp halecakir/turkish_nlp_toolkit
+docker run -p 80:8501 -d --name turkish_nlp halecakir/turkish_nlp_toolkit
